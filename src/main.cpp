008002d13fba95f4bb40fcc7396c8749ec33c5b7
Ammar Qammaz <ammarkov@gmail.com>
2012-09-15 02:56:43 +0300

    adapted to new username/password changing calls

--- src/main.cpp
+++ src/main.cpp
@@ -242,8 +242,13 @@ int main(int argc, char *argv[])
       //We were able to start camera so lets start everything else
 
       AmmServer_Start(bindIP,port,0,webserver_root,templates_root);
-      AmmServer_SetIntSettingValue(AMMSET_PASSWORD_PROTECTION,1);
-      AmmServer_SetStrSettingValue(AMMSET_PASSWORD_STR,(char *) "46YW1tYXI=");
+
+
+     AmmServer_SetStrSettingValue(AMMSET_USERNAME_STR,"admin");
+     AmmServer_SetStrSettingValue(AMMSET_PASSWORD_STR,"ammar"); //these 2 calls should change BASE64PASSWORD to YWRtaW46YW1tYXI= in
+    /* It is best to enable password protection after correctly setting both username and password
+       to avoid the rare race condition of logging only with username ( i.e. when password hasn't been declared */
+     AmmServer_SetIntSettingValue(AMMSET_PASSWORD_PROTECTION,1);
 
       init_dynamic_pages(); // Map index.html , cam.jpg to their content and callbacks
 
