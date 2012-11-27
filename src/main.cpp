b9472a2ff665abd2615894cecca5a2b60475a049
Ammar Qammaz <ammarkov@gmail.com>
2012-11-27 02:46:43 +0200

    adapted to new AmmarServer API , should work better with newer versions of VideoInput..

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include <time.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <signal.h>
 #include "../AmmarServer/src/AmmServerlib/AmmServerlib.h"
 
 #define MAX_BINDING_PORT 65534
@@ -48,7 +49,7 @@ char templates_root[MAX_FILE_PATH]="public_html/templates/";
 
 pthread_mutex_t refresh_jpeg_lock;
 
-struct AmmServer_Instance v4l2_server={0};
+struct AmmServer_Instance * v4l2_server=0;
 
 struct AmmServer_RH_Context index_page= {0};
 
@@ -92,10 +93,10 @@ void * prepare_index_page_callback(unsigned int ignored_associated_vars)
       strcat(index_page.content,"<br><br><center><img src=\"cam.jpg?i=0\" id=\"LiveImage\"><br>");
       strcat(index_page.content,"<h5>Active Clients : ");
 
-      if (AmmServer_GetInfo(&v4l2_server,AMMINF_ACTIVE_CLIENTS) < 10000 )
+      if (AmmServer_GetInfo(v4l2_server,AMMINF_ACTIVE_CLIENTS) < 10000 )
            {
              char viewers_str[150]={0};
-             sprintf(viewers_str,"%u",AmmServer_GetInfo(&v4l2_server,AMMINF_ACTIVE_CLIENTS));
+             sprintf(viewers_str,"%u",AmmServer_GetInfo(v4l2_server,AMMINF_ACTIVE_CLIENTS));
              strcat(index_page.content,viewers_str);
            } else
            {
@@ -183,17 +184,17 @@ int close_camera()
 void init_dynamic_pages()
 {
   //int AmmServer_AddResourceHandler(struct AmmServer_RH_Context * context, char * resource_name , char * web_root, unsigned int allocate_mem_bytes,unsigned int callback_every_x_msec,void * callback);
-  AmmServer_AddResourceHandler(&v4l2_server,&index_page,(char *) "/index.html",webserver_root,4096,0,(void *) &prepare_index_page_callback);
+  AmmServer_AddResourceHandler(v4l2_server,&index_page,(char *) "/index.html",webserver_root,4096,0,(void *) &prepare_index_page_callback);
 
   //Do not empty jpeg_picture struct since mallocs have already happened.. memset(&jpeg_picture,0,sizeof(struct AmmServer_RH_Context));
 
-  AmmServer_AddResourceHandler(&v4l2_server,&jpeg_picture,(char *) "/cam.jpg",webserver_root,jpg_width * jpg_height * 3, 250 /*Poll camera no sooner than once every x ms*/,(void *) &prepare_camera_data_callback);
+  AmmServer_AddResourceHandler(v4l2_server,&jpeg_picture,(char *) "/cam.jpg",webserver_root,jpg_width * jpg_height * 3, 250 /*Poll camera no sooner than once every x ms*/,(void *) &prepare_camera_data_callback);
   jpeg_picture.content_size=jpg_width * jpg_height * 3;
   jpeg_picture.MAX_content_size=jpg_width * jpg_height * 3;
 
   prepare_camera_data_callback(0); //Do a callback to populate content..!
 
-  AmmServer_DoNOTCacheResourceHandler(&v4l2_server,&jpeg_picture);  // Cam.jpg will be constantly changing , do not send 304 NOT MODIFIED
+  AmmServer_DoNOTCacheResourceHandler(v4l2_server,&jpeg_picture);  // Cam.jpg will be constantly changing , do not send 304 NOT MODIFIED
 }
 
 void close_dynamic_pages()
@@ -202,9 +203,25 @@ void close_dynamic_pages()
 }
 
 
+void termination_handler (int signum)
+     {
+        fprintf(stderr,"Terminating V4L2ToHTTP.. ");
+        close_dynamic_pages();
+        AmmServer_Stop(v4l2_server);
+        fprintf(stderr,"done\n");
+        exit(0);
+     }
+
+
+
+
 int main(int argc, char *argv[])
 {
-  printf("Ammar Server starting up\n");
+  printf("V4L2ToHTTP starting up linked to Ammar Server v%s\n",AmmServer_Version());
+  if (signal(SIGINT, termination_handler) == SIG_ERR)   printf("Cannot handle SIGINT!\n");
+  if (signal(SIGHUP, termination_handler) == SIG_ERR)   printf("Cannot handle SIGHUP!\n");
+  if (signal(SIGTERM, termination_handler) == SIG_ERR)  printf("Cannot handle SIGTERM!\n");
+  if (signal(SIGKILL, termination_handler) == SIG_ERR)  printf("Cannot handle SIGKILL!\n");
 
   char bindIP[MAX_INPUT_IP];
   strcpy(bindIP,"0.0.0.0");
@@ -245,20 +262,21 @@ int main(int argc, char *argv[])
     {
       //We were able to start camera so lets start everything else
 
-      AmmServer_Start(&v4l2_server,bindIP,port,0,webserver_root,templates_root);
+   v4l2_server = AmmServer_Start(bindIP,port,0,webserver_root,templates_root);
+   if (!v4l2_server) { fprintf(stderr,"Could not start webserver closing everything.."); close_camera(); exit(1); }
 
   if (ENABLE_PASSWORD_PROTECTION)
    {
-     AmmServer_SetStrSettingValue(&v4l2_server,AMMSET_USERNAME_STR,(char*) "admin");
-     AmmServer_SetStrSettingValue(&v4l2_server,AMMSET_PASSWORD_STR,(char*) "admin"); //these 2 calls should change BASE64PASSWORD to the proper hex value
+     AmmServer_SetStrSettingValue(v4l2_server,AMMSET_USERNAME_STR,(char*) "admin");
+     AmmServer_SetStrSettingValue(v4l2_server,AMMSET_PASSWORD_STR,(char*) "admin"); //these 2 calls should change BASE64PASSWORD to the proper hex value
     /* It is best to enable password protection after correctly setting both username and password
        to avoid the rare race condition of logging only with username ( i.e. when password hasn't been declared */
-     AmmServer_SetIntSettingValue(&v4l2_server,AMMSET_PASSWORD_PROTECTION,1);
+     AmmServer_SetIntSettingValue(v4l2_server,AMMSET_PASSWORD_PROTECTION,1);
    }
 
       init_dynamic_pages(); // Map index.html , cam.jpg to their content and callbacks
 
-      while (AmmServer_Running(&v4l2_server))
+      while (AmmServer_Running(v4l2_server))
         {
           //Serving pages on background..!
           usleep(10000);
@@ -268,7 +286,7 @@ int main(int argc, char *argv[])
 
       close_camera();
 
-      AmmServer_Stop(&v4l2_server);
+      AmmServer_Stop(v4l2_server);
     }
 
 
